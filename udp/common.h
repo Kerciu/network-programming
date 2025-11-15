84f43742113e6f1656d00e1349f3f0bcfc548d14
Kerciu <kerciuuu@gmail.com>
2025-11-16 00:04:57 +0100

    set up C project

---
+++ udp/common.h
@@ -0,0 +1,8 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define SERVER_HOST "127.0.0.1"
+#define SERVER_PORT 2137
+#define BUFFER_SIZE 1024
+
+#endif // COMMON_H
---
+++ udp/udp-server.c
@@ -0,0 +1,5 @@
+#define SERVER_HOST "127.0.0.1"
+#define SERVER_PORT 3721
+#define BUFFER_SIZE 1024
+
+void 
