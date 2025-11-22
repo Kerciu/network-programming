cbcfbc8c13c872949faf14f502fa7d30d23b48ad
Pedziwiatrr <michalozaurus@gmail.com>
2025-11-22 22:58:55 +0100

    updated ipv6 udp server code to match 1.1 and 1.2 style

--- udp_abp_ipv6/server/udp_server_ipv6.py
+++ udp_abp_ipv6/server/udp_server_ipv6.py
@@ -4,10 +4,12 @@ from server import Server
 import socket
 import struct
 
+
 class UDPServer(Server):
     def listen(self) -> None:
         with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM) as self.socket:
             self.socket.bind(self.params)
+
             print(f"[SERVER] Listening on {self.host}:{self.port}")
 
             last_client_addr = None
@@ -17,6 +19,8 @@ class UDPServer(Server):
             while True:
                 data, address = self.socket.recvfrom(self.buffer_size + 1)
 
+                print(f"[SERVER] Received datagram from {address}")
+
                 if len(data) < 1:
                     continue
 
@@ -24,18 +28,20 @@ class UDPServer(Server):
                 payload = data[1:]
 
                 if address == last_client_addr and seq == last_seq:
+                    print(f"[SERVER] seq duplicate: {seq}")
                     if last_response:
                         self.socket.sendto(last_response, address)
                     continue
 
                 try:
                     decoded = Datagram.decode(payload)
+                    msg = "OK"
                     print(f"[SERVER] Decoded: {decoded}")
-                    response_payload = Datagram.encode({"status": "OK"})
                 except Exception as e:
+                    msg = "ERROR"
                     print(f"[SERVER] Error: {e}")
-                    response_payload = Datagram.encode({"status": "ERROR"})
 
+                response_payload = Datagram.encode({"status": msg, "dg_size": f"{len(data)}"})
                 response = struct.pack("B", seq) + response_payload
 
                 last_client_addr = address
@@ -44,8 +50,12 @@ class UDPServer(Server):
 
                 self.socket.sendto(response, address)
 
+
+
 if __name__ == "__main__":
     SERVER_HOST = "::"
     SERVER_PORT = 2137
+
     server = UDPServer(ServerParams(host=SERVER_HOST, port=SERVER_PORT))
+
     server.listen()
