da7870bf601ee04c3de5f98e036f61410ec19f68
Pedziwiatrr <michalozaurus@gmail.com>
2025-11-23 18:51:12 +0100

    updated file with 1.1+1.2 version that improves buffer size parsing and matches server params

--- udp_abp_ipv6/server/server.py
+++ udp_abp_ipv6/server/server.py
@@ -1,16 +1,15 @@
 from params import ServerParams
+from datagram import Datagram
 
 from abc import ABC, abstractmethod
 import dataclasses
-
-BUFFER_SIZE = 1024
 
 
 class Server(ABC):
     def __init__(self, params: ServerParams):
         self.params = dataclasses.astuple(params)
         self.host, self.port = self.params
-        self.buffer_size = BUFFER_SIZE
+        self.buffer_size = Datagram.BUFFER_SIZE
 
     @abstractmethod
     def listen(self): ...
