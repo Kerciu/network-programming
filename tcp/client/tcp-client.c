4b9f7237eae7fbc09672da6c890112d36530ab1e
Pedziwiatrr <michalozaurus@gmail.com>
2025-11-28 22:50:43 +0100

    changed h_addr_list from h_addr to match old C std

--- tcp/client/tcp-client.c
+++ tcp/client/tcp-client.c
@@ -27,7 +27,7 @@ int main() {
         fprintf(stderr, "ERROR: no host found\n");
         return 1;
     }
-    memcpy((char *)&server_addr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+    memcpy((char *)&server_addr.sin_addr.s_addr, (char *)server->h_addr_list[0], server->h_length);
 
     if (connect(socketfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         fprintf(stderr, "Failed to connect\n");
